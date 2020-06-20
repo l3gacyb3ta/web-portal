91f4d6a2a5936138c7fbfa4877dfed506d763623
Bobbbay <abatterysingle@gmail.com>
2020-06-20 00:31:49 -0400

    Nav styling + functionality

--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -2,13 +2,27 @@ import styled, { css } from "styled-components";
 
 import LinkComponent from "../Link";
 
-export const Wrapper = styled.nav``;
+export const Wrapper = styled.nav`
+  display: flex;
+  align-items: center;
+  justfy-content: center;
+`;
 
-export const Logo = styled.h1``;
+export const Logo = styled.h1`
+  margin-left: 5px;
+`;
 
-export const Links = styled.ul``;
+export const Links = styled.ul`
+  display: flex;
+  align-items: center;
+  justfy-content: center;
+  margin-left: auto;
+  padding: 5px;
+`;
 
 export const LinkItem = styled.li`
+  margin: 10px;
+  list-style: none;
   ${({ feature }) =>
     feature &&
     css`
