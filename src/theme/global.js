67ca8463eb3cc47fd6b1fde8144fcae9dbb37e5a
Bobbbay <abatterysingle@gmail.com>
2020-06-20 00:32:40 -0400

    General style additions

--- src/theme/global.js
+++ src/theme/global.js
@@ -1,16 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Viga&display=swap');
   *,
   *:before,
   *:after {
     box-sizing: border-box;
   }
-
   html{
-    background-color: #1d2951;
+    background-color: #fff;
+    color: #000;
+    font-family: 'Viga', sans-serif;
   }
-
   body{
     margin: 0;
   }
